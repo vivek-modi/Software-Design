8b7c66007371e41009c87091c051e77cb6a39ad6
Vivek Modi <vivek.modi456@gmail.com>
2019-11-25 10:57:58 +0000

    Village helper Class

---
+++ Practise/src/EQ1/Village.java
@@ -0,0 +1,36 @@
+package EQ1;
+
+public class Village {
+
+	private String name, county;
+	private int population;
+
+	public Village(String name, String county, int population) {
+		super();
+		this.name = name;
+		this.county = county;
+		this.population = population;
+	}
+
+	public boolean resetPopulation(int newPop) {
+
+		if (newPop >= 0) {
+			population = newPop;
+			return true;
+		} else
+			return false;
+	}
+
+	public String readCounty() {
+		return county;
+	}
+
+	public int readPopulation() {
+		return population;
+	}
+
+	public void changeCounty(String newName) {
+		county = newName;
+	}
+
+}
