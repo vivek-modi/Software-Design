94f2bb87428c8b5f856f87f0c9da4946563846cc
Vivek Modi <vivek.modi456@gmail.com>
2019-12-10 18:06:27 +0000

    Added Aggreation File of PairDice

--- Finalpracticse/src/Q4/PairDice.java
+++ Finalpracticse/src/Q4/PairDice.java
@@ -1,12 +1,26 @@
 package Q4;
 
-public class PairDice extends Die {
-	
+public class PairDice {
+
 	private Die d1;
 	private Die d2;
 
-	public PairDice(int die) {
-		super(die);
+	public PairDice(int v1, int v2) {
+		d1 = new Die(v1);
+		d2 = new Die(v2);
+	}
+
+	public void rollDice() {
+		d1.roll();
+		d2.roll();
+	}
+
+	public int sumDiceValues() {
+		return d1.getValue() + d2.getValue();
+	}
+
+	public boolean valuesEqual() {
+		return (d1.getValue() == d2.getValue()) ? true : false;
 	}
 
 }
