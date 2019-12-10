f79b77d908d1e4d49ca3feffa67815d153400678
Vivek Modi <vivek.modi456@gmail.com>
2019-12-10 22:39:49 +0000

    Added count Digit,Count of odd digit,odd digit,sum of all digit

--- Finalpracticse/src/Q3/recursiv.java
+++ Finalpracticse/src/Q3/recursiv.java
@@ -5,12 +5,31 @@ import java.util.*;
 public class recursiv {
 
 	public static int countDigits(int value) {
+
 		if (value == 0)
 			return 0;
 		else {
+			
+			// Count Digit
 			return 1 + countDigits(value / 10);
 
-			// return (value % 10 + countDigits(value / 10));
+			/*
+			 * Count of Odd Digit
+			 * 
+			 * int last = value % 10; if (last % 2 == 1) { return 1 + countDigits(value /
+			 * 10); } else { return 0 + countDigits(value / 10); }
+			 */
+			
+			/*
+			 * Add of Odd Digit
+			 * 
+			 * int last = value % 10; if (last % 2 == 1) { return last + countDigits(value /
+			 * 10); } else { return 0 + countDigits(value / 10); }
+			 */
+
+			/*
+			 * Sum all all Digit return (value % 10 + countDigits(value / 10));
+			 */
 		}
 	}
 
